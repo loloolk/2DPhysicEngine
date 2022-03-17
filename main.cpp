475448a482900abff96f4bd346b6dce320c1f793
loloolk <marc.bondoger@gmail.com>
2022-03-17 14:00:12 -0600

    test

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+#include "time.h"
 #include "objects.cpp"
 
 
 int main() {
-    //side length, position
-    square s3(2, mtn::Vector2(-1.0, 0.0));
+    auto begin = std::chrono::high_resolution_clock::now();
 
     //side length, position
-    rectangle s1(2, 2, mtn::Vector2(1.0, 0.0)); //1, 2
+    square s1(2, mtn::Vector2(-1.0, 0.0));
+
+    //side length, position
+    rectangle s2(2, 2, mtn::Vector2(1.0, 0.0)); //1, 2
 
     //rad, position
-    circle s2(1, mtn::Vector2(3.0, 0.0)); //1, 2
+    circle s3(1, mtn::Vector2(3.0, 0.0)); //1, 2
 
     //s1.velocity = mtn::Vector2();
     //s2.velocity = mtn::Vector2();
@@ -24,6 +28,11 @@ int main() {
         std::cout << "s1: " << s1.position << ", s2: " << s2.position << ", s3: " << s3.position << std::endl;
         update();
     }
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    std::cout << "Time taken: " << elapsed.count() << "ms" << std::endl;
 }
 
 //only switch momentum if same mass
