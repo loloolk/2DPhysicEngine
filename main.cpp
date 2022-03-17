ee4f8a97d426f879bb65a61faa1dbb1ce44205d8
loloolk <marc.bondoger@gmail.com>
2022-03-17 14:47:23 -0600

    test

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <chrono>
 #include "time.h"
@@ -6,7 +5,6 @@
 
 
 int main() {
-    auto begin = std::chrono::high_resolution_clock::now();
 
     //side length, position
     square s1(2, mtn::Vector2(-1.0, 0.0));
@@ -16,20 +14,28 @@ int main() {
 
     //rad, position
     circle s3(1, mtn::Vector2(3.0, 0.0)); //1, 2
+    circle s5(1, mtn::Vector2(3.0, 0.0)); //1, 2
+    circle s6(1, mtn::Vector2(3.0, 0.0)); //1, 2
+    circle s7(1, mtn::Vector2(3.0, 0.0)); //1, 2
+    circle s8(1, mtn::Vector2(3.0, 0.0)); //1, 2
+    circle s9(1, mtn::Vector2(3.0, 0.0)); //1, 2
+    circle s10(1, mtn::Vector2(3.0, 0.0)); //1, 2
+    circle s4(1, mtn::Vector2(3.0, 0.0)); //1, 2
+    circle s11(1, mtn::Vector2(3.0, 0.0)); //1, 2
+    circle s12(1, mtn::Vector2(3.0, 0.0)); //1, 2
 
     //s1.velocity = mtn::Vector2();
     //s2.velocity = mtn::Vector2();
 
     //s3.acceleration = mtn::Vector2(5.0, -3.0);
 
-    std::cout << findClosestObject(s1.position) << std::endl;
-
+    auto begin = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 10; i++) {
         std::cout << "s1: " << s1.position << ", s2: " << s2.position << ", s3: " << s3.position << std::endl;
         update();
     }
-
     auto end = std::chrono::high_resolution_clock::now();
+
 
     auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
     std::cout << "Time taken: " << elapsed.count() << "ms" << std::endl;
