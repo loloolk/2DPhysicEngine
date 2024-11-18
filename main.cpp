8a39880c3e5e48b9bdedba3015dfa392ebb4b0f5
loloolk <marc.bondoger@gmail.com>
2024-11-18 14:13:32 -0800

    Removed unecessary timer

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <string>
-#include <chrono>
 #include "time.h"
 #include "logic.cpp"
 
 int main() {
-    auto begin1 = std::chrono::high_resolution_clock::now();
     circle c1(1, mtn::Vector2(-50000.0, 0.0), mtn::Vector2(5.0, 0.0));
     circle c2(1, mtn::Vector2(0.0, 0.0));
     square s1(2, mtn::Vector2(0.1, 0.0));
@@ -17,6 +15,4 @@ int main() {
     for (int i = 0; i < 10; i++) {
         update();
     }
-    auto end1 = std::chrono::high_resolution_clock::now();
-    std::cout << "first section: " << std::chrono::duration_cast<std::chrono::milliseconds>(end1 - begin1).count() << " miliseconds" << std::endl;
 }
